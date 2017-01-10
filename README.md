7c36cbfbae8ed8e79d3621490fc5eb992dae201f
runnerdave <runnerdave@gmail.com>
2017-01-11 06:33:51 +1100

    finished assessment and improved the README file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SQL practice
-* Sakila database is used in may examples
+* Sakila database is used in may examples, the schema is shown in the file: Database-Schema.png
+* Postgres database used, in personal mac installed with homebrew
+* For database admin tasks pgAdmin3 is used
+* To load the Sakila database use the file: dvdrental.tar restoring it into a new database
+* The .sql files are:
+  * sql_bootcamp.sql, contains sql scripts used through the bootcamp, from selects all the way to joins
+  * assessment2-bookings.sql, contains the solutions to the assessment 2, the questions are in the script as comments.
+    * to solve the assessment2 create a database in postgres and load the exercises-assessment-2.tar file using a restore command,
+      it does not use the Sakila database but a bookings database
--- assessment-3.sql
+++ assessment-3.sql
@@ -2,10 +2,7 @@
 Create a new database called "School" this database should have two tables: teachers and students.
 
 The students table should have columns for student_id, first_name,last_name, homeroom_number, phone,email, and graduation year.
-
-The teachers table should have columns for teacher_id, first_name, last_name,
-
-homeroom_number, department, email, and phone.
+The teachers table should have columns for teacher_id, first_name, last_name, homeroom_number, department, email, and phone.
 
 The constraints are mostly up to you, but your table constraints do have to consider the following:
 
@@ -21,3 +18,39 @@ His contact info is: jsalk@school.org and a phone number of 777-555-4321.
 Keep in mind that these insert tasks may effect your constraints!
 
 */
+DROP TABLE IF EXISTS teachers;
+
+CREATE TABLE teachers (
+	teacher_id serial PRIMARY KEY, 
+	first_name varchar(65) NOT NULL, 
+	last_name varchar(65) NOT NULL, 
+	homeroom_number char(10), 
+	department varchar(25), 
+	email varchar(50) UNIQUE NOT NULL, 
+	phone char (12) UNIQUE NOT NULL
+);
+
+DROP TABLE IF EXISTS students CASCADE;
+
+CREATE TABLE students (
+	student_id serial PRIMARY KEY, 
+	first_name varchar(65) NOT NULL, 
+	last_name varchar(65) NOT NULL, 
+	homeroom_number char(10), 
+	graduation_year int, 
+	email varchar(50) UNIQUE, 
+	phone char (12) UNIQUE NOT NULL
+);
+
+INSERT INTO students (first_name, last_name, homeroom_number, graduation_year, phone) 
+VALUES ('Mark', 'Watney', '5', '2035', '777-555-1234');
+
+INSERT INTO teachers (first_name, last_name, homeroom_number, department, email, phone) 
+VALUES ('Jonas', 'Salk', '5', 'Biology', 'jsalk@school.org', '777-555-4321');
+
+SELECT * FROM students;
+SELECT * FROM teachers;
+
+CREATE VIEW all_students AS SELECT * FROM students;
+
+SELECT * FROM all_students;
